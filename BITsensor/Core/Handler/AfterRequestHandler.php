5093dc20f01534c11f9d56ad56568bbacd6dbf3b
Ruben van Vreeland <rubenvanvreeland@gmail.com>
2015-02-20 14:14:09 +0100

    Fatal Error Catcher

--- BITsensor/Core/Handler/AfterRequestHandler.php
+++ BITsensor/Core/Handler/AfterRequestHandler.php
@@ -13,9 +13,30 @@ class AfterRequestHandler
         if(AfterRequestHandler::$executed === true)
             return;
         
+        AfterRequestHandler::fatalHandler();
+        
         SqlErrorHandler::Handle();
         ReportingHandler::Handle();
         
         AfterRequestHandler::$executed = true;
+    }
+    
+    private static function fatalHandler() 
+    {
+        $errfile = "unknown file";
+        $errstr  = "shutdown";
+        $errno   = E_CORE_ERROR;
+        $errline = 0;
+
+        $error = error_get_last();
+
+        if( $error !== NULL) {
+          $errno   = $error["type"];
+          $errfile = $error["file"];
+          $errline = $error["line"];
+          $errstr  = $error["message"];
+
+          CodeErrorHandler::Handle($errno, $errstr, $errfile, $errline);
+        }
     }
 }
--- BITsensor/Core/Handler/RequestInputHandler.php
+++ BITsensor/Core/Handler/RequestInputHandler.php
@@ -5,7 +5,7 @@ class RequestInputHandler {
     public static function Handle() {
         RequestInputHandler::ProcessIntput($_POST, array('HTTP', 'Post'));
         RequestInputHandler::ProcessIntput($_GET, array('HTTP', 'Get'));
-        RequestInputHandler::ProcessIntput($_COOKIE, array('HTTP', 'Cookie'));
+        //RequestInputHandler::ProcessIntput($_COOKIE, array('HTTP', 'Cookie'));
     }
 
     private static function ProcessIntput($value, $name) {
