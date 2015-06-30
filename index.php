4e80e9f27c131aa6820a4890d20d37cac68bd986
Ruben van Vreeland <rubenvanvreeland@gmail.com>
2015-06-30 23:06:53 +0200

    Ignore

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <?php
     global $BITsensor;
     
-    $BITsensor->AddContext(\BITsensor\Core\Context::User('Ruben BadUser'));
+    //$BITsensor->AddContext(\BITsensor\Core\Context::User('Ruben BadUser'));
 
     
     print_r($_SERVER);
     /* @var $BITsensor Collector 
     
-    $BITsensor->AddError(new SqlError(12, 'testsql'));
+    $BITsensor->AddError(new SqlError(12, 'testsql'));*/
 
     $detection = new Detection('File Uploader');
         $detection->addRule(new DetectionRule(1, 'Malicious file extension', 1, 'File Upload' ));
-        $BITsensor->AddDetection($detection);*/
+        $BITsensor->AddDetection($detection);
 ?>
