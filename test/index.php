f2f1eadd3b63b9d391b12f2f06052dfe3a37f6be
Sven Dubbeld <sven.dubbeld1@gmail.com>
2015-08-09 17:42:52 +0200

    Show page when user gets blocked

--- test/index.php
+++ test/index.php
@@ -3,8 +3,6 @@ use BitSensor\Core\BitSensor;
 
 require_once '../vendor/autoload.php';
 
-header('Content-Type: application/json');
-
 new BitSensor('http://localhost/test/api/', 'example_user', 'abcdefghijklmnopqrstuvwxyz');
 
 echo 'Allowed';
