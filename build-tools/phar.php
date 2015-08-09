e357485cfdc8a2387882923de1049d6d5dee496a
Sven Dubbeld <sven.dubbeld1@gmail.com>
2015-08-09 17:49:58 +0200

    Remove old archives before creating new ones

--- build-tools/phar.php
+++ build-tools/phar.php
@@ -4,27 +4,44 @@ require_once 'version.php';
 
 if (isset($argv[1])) {
 
-    echo "Building $argv[1] \n";
+    $out = $argv[1];
+
+    echo "Building $out \n";
     echo "Version: " . BITSENSOR_BUILD_VERSION . "\n";
 
     echo "\n";
-    $phar = new Phar($argv[1], 0);
-    echo "Adding files...\n";
-    $phar->buildFromDirectory('src/');
-    echo "Setting default stub...\n";
-    $phar->setStub($phar->createDefaultStub('index.php'));
 
-    echo "Adding metadata...\n";
+    echo "Removing old archives...";
+    @unlink($out);
+    @unlink($out . '.bz2');
+    @unlink($out . '.gz');
+    echo "done\n";
+
+    echo "Creating new archive...";
+    $phar = new Phar($out, 0);
+    echo "done\n";
+
+    echo "Adding files...";
+    $phar->buildFromDirectory('src/');
+    echo "done\n";
+
+    echo "Setting default stub...";
+    $phar->setStub($phar->createDefaultStub('index.php'));
+    echo "done\n";
+
+    echo "Adding metadata...";
     $phar->setMetadata(array(
         'version' => BITSENSOR_BUILD_VERSION,
         'buildDate' => date('Y-m-d H:i:s'),
         'title' => 'BitSensor Web Application Security',
         'company' => 'BitSaver'
     ));
+    echo "done\n";
 
-    echo "Compressing archive...\n";
+    echo "Compressing archive...";
     $phar->compress(Phar::BZ2);
     $phar->compress(Phar::GZ);
+    echo "done\n";
 
     echo "Build finished.\n";
 
