fb5379fa5b4a112a619f68ce306a399430bab729
Sven Dubbeld <sven.dubbeld1@gmail.com>
2015-08-09 16:35:22 +0200

    Added metadata

--- build-tools/phar.php
+++ build-tools/phar.php
@@ -1,9 +1,27 @@
 <?php
 
+require_once 'version.php';
+
 if (isset($argv[1])) {
 
+    echo "Building $argv[1] \n";
+    echo "Version: " . BITSENSOR_BUILD_VERSION . "\n";
+
+    echo "\n";
     $phar = new Phar($argv[1], 0);
+    echo "Adding files...\n";
     $phar->buildFromDirectory('src/');
+    echo "Setting default stub...\n";
     $phar->setStub($phar->createDefaultStub('index.php'));
+
+    echo "Adding metadata...\n";
+    $phar->setMetadata(array(
+        'version' => BITSENSOR_BUILD_VERSION,
+        'buildDate' => date('Y-m-d H:i:s'),
+        'title' => 'BitSensor PHP Plugin',
+        'company' => 'BitSaver'
+    ));
+
+    echo "Build finished.\n";
 
 }
