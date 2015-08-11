d00d64cc3b3b40f8136831b730d75053b81fb377
Ruben van Vreeland <rubenvanvreeland@gmail.com>
2015-08-11 21:52:27 +0200

    Elastic Index

--- test/index.php
+++ test/index.php
@@ -3,6 +3,6 @@ use BitSensor\Core\BitSensor;
 
 require_once '../vendor/autoload.php';
 
-new BitSensor('http://localhost/test/api/', 'example_user', 'abcdefghijklmnopqrstuvwxyz');
+new BitSensor('http://192.168.159.131:9200/demo/datapoint/', 'example_user', 'abcdefghijklmnopqrstuvwxyz');
 
 echo 'Allowed';
