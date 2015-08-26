ca7562acb5a23a4b267bc5cf2811189721cbd3a0
Sven Dubbeld <sven.dubbeld1@gmail.com>
2015-08-26 22:28:20 +0200

    Let client chain own error documents after ours

--- src/Handler/ErrorDocumentHandler.php
+++ src/Handler/ErrorDocumentHandler.php
@@ -3,9 +3,17 @@
 use BitSensor\Core\ApacheError;
 use BitSensor\Core\BitSensor;
 
-echo '<h1>' . $_GET['e'] . '</h1>';
-
 require_once '../index.php';
+
+if (isset($_GET['e'])) {
+    $path = getenv('ERROR_DOCUMENT_' . $_GET['e']);
+    if ($path && file_exists($path)) {
+        /** @noinspection PhpIncludeInspection */
+        include $path;
+    } else {
+        echo '<h1>' . $_GET['e'] . '</h1>';
+    }
+}
 
 $bitSensor = new BitSensor();
 
