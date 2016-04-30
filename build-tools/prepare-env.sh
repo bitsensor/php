fd816d7c1218febd4c548f32c190d21c88a54934
Sven Dubbeld <sven@bitsensor.io>
2016-04-30 19:06:11 +0200

    Change default version to 5.6

--- build-tools/prepare-env.sh
+++ build-tools/prepare-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=${1:-"5.5"}
+VERSION=${1:-"5.6"}
 export PATH="/opt/phpenv/shims:/opt/phpenv/bin:/opt/php-build/bin/:/opt/composer/vendor/bin:${PATH}"
 phpenv global $VERSION
 sed -i -- 's/;phar.readonly = On/phar.readonly = Off/g' /opt/phpenv/versions/$VERSION/etc/php.ini
