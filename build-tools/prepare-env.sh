c1ee95d5481e4cd7c4c58d8e7a9f932aeffb5eff
Sven Dubbeld <sven@bitsensor.io>
2016-04-30 17:52:41 +0200

    Change pecl channel

--- build-tools/prepare-env.sh
+++ build-tools/prepare-env.sh
@@ -3,4 +3,5 @@ VERSION=${1:-"5.5"}
 export PATH="/opt/phpenv/shims:/opt/phpenv/bin:/opt/php-build/bin/:/opt/composer/vendor/bin:${PATH}"
 phpenv global $VERSION
 sed -i -- 's/;phar.readonly = On/phar.readonly = Off/g' /opt/phpenv/versions/$VERSION/etc/php.ini
+pecl channel-update pear.php.net
 pecl install bz2
