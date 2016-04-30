c4a5fa7e00b1b5f037c851ddb7a73262a89160f2
Sven Dubbeld <sven@bitsensor.io>
2016-04-30 17:40:55 +0200

    Export path

--- build-tools/prepare-env.sh
+++ build-tools/prepare-env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VERSION=${1:-"5.5"}
 PATH="/opt/phpenv/shims:/opt/phpenv/bin:/opt/php-build/bin/:/opt/composer/vendor/bin:$PATH"
+export PATH
 phpenv global $VERSION
 sed -i -- 's/;phar.readonly = On/phar.readonly = Off/g' /opt/phpenv/versions/$VERSION/etc/php.ini
