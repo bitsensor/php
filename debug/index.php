73109e821c8863e2eebe2f09518821631db441ac
root <root@lamp.(none)>
2016-05-12 12:26:15 +0000

    Create debug folder with debug file

---
+++ debug/index.php
@@ -0,0 +1,29 @@
+<?php
+use BitSensor\Core\BitSensor;
+use BitSensor\Core\Config;
+use BitSensor\Core\SessionContext;
+
+require_once '../target/bitsensor.phar';
+
+global $debug;
+$debug = true;
+
+$config = new Config();
+$config->setUri('http://localhost:8080/');
+$config->setUser('example_user');
+$config->setApiKey('abcdefghijklmnopqrstuvwxyz');
+$config->setMode(Config::MODE_DETECTION);
+$config->setConnectionFail(Config::ACTION_ALLOW);
+$config->setIpAddressSrc(Config::IP_ADDRESS_REMOTE_ADDR);
+$config->setLogLevel(Config::LOG_LEVEL_ALL);
+
+$bitSensor = new BitSensor($config);
+
+session_start();
+
+$_SESSION['user'] = 'le moi';
+
+$bitSensor->addContext(new SessionContext(SessionContext::USERNAME, $_SESSION['user']));
+
+$bitSensor->addError(new \BitSensor\Core\CodeError(1, "a", "b.php", 2, null, "Custom"));
+$bitSensor->addError(new \BitSensor\Core\CodeError(1, "a", "b.php", 2, null, "Custom"));
