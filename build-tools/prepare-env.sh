d634c0fe5aff4ff7249db8a64a79b748f642e6f1
Martijn Rondeel <martijn@rondeel.email>
2017-11-22 17:40:46 +0100

    Update prepare-env.sh

--- build-tools/prepare-env.sh
+++ build-tools/prepare-env.sh
@@ -4,4 +4,5 @@ export PATH="/opt/phpenv/shims:/opt/phpenv/bin:/opt/php-build/bin/:/opt/composer
 phpenv global $VERSION
 sed -i -- 's/;phar.readonly = On/phar.readonly = Off/g' /opt/phpenv/versions/$VERSION/etc/php.ini
 composer self-update
+composer config cache-files-dir ../.composercache
 composer install --no-progress
