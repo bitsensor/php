10931c6892d5cd45fdaddc750aa9e2a8afdacdcb
Ruben van Vreeland <ruben@bitsensor.io>
2017-12-08 16:36:18 +0100

    Update location of php.ini

--- build-tools/prepare-env.sh
+++ build-tools/prepare-env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PATH="/home/ubuntu/.phpenv/shims:/home/ubuntu/.phpenv/bin:/home/ubuntu/.phpenv/bin:${PATH}"
 phpenv version
+sed -i -- 's/;phar.readonly = On/phar.readonly = Off/g' /home/ubuntu/.phpenv/versions/*/etc/php.ini
 composer self-update
 composer install --no-progress
