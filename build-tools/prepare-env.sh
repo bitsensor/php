fdef15073ef3c174841026d6b1b579f539d656e7
Ruben van Vreeland <ruben@bitsensor.io>
2017-12-08 16:07:35 +0100

    Revert prepare-env that is only suited for local

--- build-tools/prepare-env.sh
+++ build-tools/prepare-env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export PATH="/home/ubuntu/.phpenv/shims:/home/ubuntu/.phpenv/bin:/home/ubuntu/.phpenv/bin:${PATH}"
-phpenv version
+VERSION=${1:-"5.6"}
+export PATH="/opt/phpenv/shims:/opt/phpenv/bin:/opt/php-build/bin/:/opt/composer/vendor/bin:${PATH}"
+phpenv global $VERSION
+sed -i -- 's/;phar.readonly = On/phar.readonly = Off/g' /opt/phpenv/versions/$VERSION/etc/php.ini
 composer self-update
 composer install --no-progress
