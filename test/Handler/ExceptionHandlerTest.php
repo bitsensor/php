f414dfcee990698303613bb6c0a4ff114107b308
Martijn Rondeel <martijn@rondeel.email>
2018-10-16 11:45:54 +0200

    Fix CI

--- test/Handler/ExceptionHandlerTest.php
+++ test/Handler/ExceptionHandlerTest.php
@@ -25,8 +25,8 @@ class ExceptionHandlerTest extends HandlerTest
         /** @var Error $err */
         $err = BitSensor::getDatapoint()->getErrors()[0];
 
-        Log::info(print_r($err->getContext(), true));
-        Log::info(print_r($expectedContext, true));
+        echo print_r($err->getContext(), true);
+        echo print_r($expectedContext, true);
 
         self::assertEquals($err->getCode(), $code);
         self::assertEquals($err->getDescription(), $message);
